93fcfd9cfc693be690c6fdf4f17976c8d0b241f2
Thu <Thu601925@gmail.com>
2025-03-02 15:08:05 +0700

    add

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -5,12 +5,27 @@ import Product from "../components/Product";
 import { useDispatch } from "react-redux";
 import { fetchProduct } from "../store/actions/fetchProduct";
 import TopProduct from "../components/TopProduct";
+import { setupAxiosInterceptors } from "../utils/api";
+import { useAuth } from "../context/AuthContext";
+import { fetchUser } from "../store/actions/fetchUser";
+import { fetchCart } from "../store/actions/fetchCart";
 
 export default function Home() {
-  const dispath = useDispatch();
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispath(fetchProduct({ _limit: 20, _page: 1, is_active: true }));
+    dispatch(fetchProduct({ _limit: 20, _page: 1, is_active: true }));
   }, []);
+
+  const { logout } = useAuth();
+
+  useEffect(() => {
+    setupAxiosInterceptors(logout);
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      dispatch(fetchUser());
+      dispatch(fetchCart(token));
+    }
+  }, [dispatch, logout]);
   return (
     <div className="w-full gap-4">
       <HomeBanner />
